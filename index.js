5bb50ec408731e247b00fb098d124f4f2aae3e85
GASADER <kajorhkaen@gmail.com>
2023-04-26 14:04:29 +0700

    Add.npm

---
+++ index.js
@@ -0,0 +1,10 @@
+const qrcode = require('qrcode')
+const generatePayload = require('promptpay-qr')
+const prompt = require('prompt-sync')()
+
+const mobileNumber = prompt('How many for payment? (xxx-xxx-xxxx)')
+const amount = +prompt('How many for payment?')
+console.log(amount)
+const payload = generatePayload(mobileNumber, { amount })
+// qrcode.generate(payload)
+console.log(payload)
